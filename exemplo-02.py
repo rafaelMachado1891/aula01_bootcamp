fb5d18e411e917e80be3a35e55d81d874bf25511
rafaelMachado1891 <rafadmn1@gmail.com>
2024-06-16 21:50:28 -0300

    adicionando exemplo-02

---
+++ exemplo-02.py
@@ -0,0 +1,8 @@
+# Crie um programa onde o usuário entra com dois valores e o resultado seja a soma
+primeiro_valor = input("Entre com o valor inicial: ")
+
+segundo_valor = input("Entre com o segundo valor: ")
+
+resultado = int(primeiro_valor) + int(segundo_valor)
+
+print("A soma dos valores é: " ,resultado)
