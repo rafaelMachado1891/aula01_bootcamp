d9c64c7e0d9b7a059e41280e1100f81497eb5f69
rafaelMachado1891 <rafadmn1@gmail.com>
2024-06-16 21:49:13 -0300

    adicionando desafio

---
+++ desafio_01.py
@@ -0,0 +1,11 @@
+nome = input("Digite o seu nome: ")
+
+salario = float(input("Digite o seu salário: "))
+
+bonus_valor = float(input("Qual foi o valor do seu bonus? "))
+
+bonus = 1000 + salario * bonus_valor
+
+print("Seja bem vindo ",nome)
+
+print("O valor total do seu bonus é: ", bonus)
