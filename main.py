7971f1e272024d72ba669379b1ba6b50f123e86c
rafaelMachado1891 <rafadmn1@gmail.com>
2024-06-16 21:51:17 -0300

    atualizando arquivo main

---
+++ main.py
@@ -0,0 +1,9 @@
+# criando um progama em que o usuario entra com o seu nome e o retorno é o número de caracteres
+
+# print(len(input("Digite o seu nome: ")))
+
+nome = input("Digite seu nome: ")
+
+quantidade_caracteres = len(nome)
+
+print(quantidade_caracteres)
